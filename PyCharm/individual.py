bd9db07d32c691681577d91225e05e36b90263a8
student-09-525 <student-09-525@ncfu.net>
2022-10-29 11:29:56 +0300

    идндивидуальное задание

---
+++ PyCharm/individual.py
@@ -0,0 +1,6 @@
+from math import sqrt
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+sred = (a+b)/2
+geom = sqrt(a*b)
+print("Среднее арифметическое: ", sred,"\nСреднее геометрическое: ", geom)
