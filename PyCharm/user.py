8bfa895c3b54e7239a29c9156a951841526ea90b
AndrejMirrox <sotnikoff.andr@gmail.com>
2022-10-29 09:29:26 +0300

    добавил 2 программы

---
+++ PyCharm/user.py
@@ -0,0 +1,4 @@
+name = str(input("What is your name? "))
+age = int(input("How old are you? "))
+place = str(input("Where are you live? "))
+print("\n","This is ",name,"\n","It is ", age , "\n", "He live in ", place)
