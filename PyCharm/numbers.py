b47417022e206ccf74adaebb92ec531a155c5252
AndrejMirrox <sotnikoff.andr@gmail.com>
2022-10-29 09:44:17 +0300

    ещё некоторые файлы

---
+++ PyCharm/numbers.py
@@ -0,0 +1,5 @@
+a, b, c, d = map(int, input("Введите 4 числа: ").split())
+sum1 = a + b
+sum2 = c + d
+chast = sum1/sum2
+print ("\n","Частное из суммы первых двух чисел и суммы последующих двух = ",'%.2f' % chast)
